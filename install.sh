1bec635a1217611eb265f816f7281c1379e7274c
Hiroaki Goto <goto.inct@gmail.com>
2024-03-11 03:22:55 +0900

    Change install.sh to use install command

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-set -x
-
-cp $(find scripts -type f) "${HOME}/.local/bin/"
+INSTALL_DIRECTORY="${HOME}/.local/bin/"
+echo -e "Install scripts into \"${INSTALL_DIRECTORY}\"\n"
+install --verbose --preserve-timestamps $(find scripts -type f) "${INSTALL_DIRECTORY}"
